8865c717fad5ef7964637e888c5d979c9e3d468b
Cloyce Stetson <crs170030@utdallas.edu>
2021-10-18 20:53:09 -0500

    UI Tutorial
    Added UI from class notes and beginning scriptable objects.

---
+++ Assets/Scripts/DialogueView.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueView : MonoBehaviour
+{
+    [SerializeField] GameObject _panel;
+    [SerializeField] Text _dialogueTextUI;
+    [SerializeField] Text _characterNameTextUI;
+    [SerializeField] Image _characterPortraitUI;
+
+    Vector2 showPivot = new Vector2(0f, 0f);
+    Vector2 hidePivot = new Vector2(0f, 0f);
+
+    float moveSpeed = 0.01f;
+    bool movingUp = false;
+
+    void Start()
+    {
+        CloseDisplay();
+        //_panel.transform.position.y = _panel.transform.position.y - 1000;// * Vector3.down * 1000;
+    }
+
+    void FixedUpdate()
+    {
+        if(movingUp = true)
+        {
+            if(_panel.transform.position.y < transform.position.y)
+                _panel.transform.Translate(Vector3.up * moveSpeed);
+        }
+        else
+        {
+            if (_panel.transform.position.y > transform.position.y-1000)
+                _panel.transform.Translate(Vector3.down * moveSpeed);
+        }
+    }
+
+    public void Display(DialogueData data)
+    {
+        //enable the panel
+        _panel.SetActive(true);
+        //set panel data
+        _dialogueTextUI.text = data.Dialogue;
+        _characterNameTextUI.text = data.CharacterName;
+        _characterPortraitUI.sprite = data.Portrait;
+
+        Debug.Log("Panel x = " + _panel.transform.position.x + "Panel y = " + _panel.transform.position.y);
+        //move the panel upwards
+        movingUp = true;
+        //while(_panel.transform.position.y < transform.position.y)
+            //_panel.transform.Translate(Vector3.up * Time.deltaTime * moveSpeed);
+    }
+
+    public void CloseDisplay()
+    {
+        //move panel downwards
+        movingUp = false;
+        //while (_panel.transform.position.y > transform.position.y-1000)
+            //_panel.transform.Translate(Vector3.down * Time.deltaTime * moveSpeed);
+
+        //disable the panel
+        _panel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/LevelTester.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTester : MonoBehaviour
+{
+    [SerializeField] DialogueView _dialogueView;
+    [SerializeField] DialogueData _dialogue01;
+    [SerializeField] DialogueData _dialogue02;
+    [SerializeField] DialogueData _dialogue03;
+
+    bool toggleUI = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (toggleUI)
+            {
+                _dialogueView.Display(_dialogue01);
+                toggleUI = false;
+            }
+            else
+            {
+                _dialogueView.CloseDisplay();
+                toggleUI = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (toggleUI)
+            {
+                _dialogueView.Display(_dialogue02);
+                toggleUI = false;
+            }
+            else
+            {
+                _dialogueView.CloseDisplay();
+                toggleUI = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (toggleUI)
+            {
+                _dialogueView.Display(_dialogue03);
+                toggleUI = false;
+            }
+            else
+            {
+                _dialogueView.CloseDisplay();
+                toggleUI = true;
+            }
+        }
+    }
+}
